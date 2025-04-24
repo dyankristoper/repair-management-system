a1201eafbdd1d01f5dde5cbbe6b622ce9c9f5577
adriantech-beep <Adhrianne29@gmail.com>
2025-04-24 18:37:46 +0800

    updated header component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import Logout from "../features/authentication/Logout";
 import UserAvatar from "../features/authentication/UserAvatar";
-import HeaderMenu from "./HeaderMenu";
 import Account from "../features/authentication/Account";
 
 const StyledHeader = styled.header`
