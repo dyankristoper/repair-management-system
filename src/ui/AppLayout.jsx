e0c9bb8c2c5c1ebdea1ac7b748560360ac663c83
John Christopher Azcarraga <stellarm_13@yahoo.com>
2025-05-14 09:08:01 +0800

    Fix: Filename issue

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar";
+import Sidebar from "./SideBar";
 import Header from "./Header";
 import styled from "styled-components";
 
