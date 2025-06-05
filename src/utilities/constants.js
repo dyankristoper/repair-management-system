d3829791ac6b8dccc87b0717732c0ffd9b6af746
John Christopher Azcarraga <stellarm_13@yahoo.com>
2025-06-05 10:05:02 +0800

    Feat: Implement error handling and logs

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -2,7 +2,14 @@ import Logo from '/logo.png';
 
 const PAGE_SIZE = 4;
 
+const LOG_TYPES = {
+  default: 'default',
+  info: 'info',
+  error: 'error'
+}
+
 export {
   Logo,
-  PAGE_SIZE
+  PAGE_SIZE,
+  LOG_TYPES
 }
