768f897ef14c89d5532bdee779ff13cd4cb9dce8
John Christopher Azcarraga <stellarm_13@yahoo.com>
2025-06-08 13:03:49 +0800

    Fix: Tailwind installation

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -5,7 +5,7 @@ const Input = styled.input`
   background-color: var(--color-grey-0);
   border-radius: var(--border-radius-sm);
   border-radius: 5px;
-  padding: 0%.8rem 1.2rem;
+  padding: 0.8rem 1.2rem;
   box-shadow: var(--shadow-sm);
 `;
 
--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,4 +1,5 @@
 import { useSettings } from "../settings/useSettings";
+import { useUser } from "../features/authentication/useUser";
 import styled from "styled-components";
 import EditIcon from "../ui/EditIcon";
 
@@ -28,14 +29,18 @@ const TitleLogo = styled.div`
 
 function Logo() {
   const { settings } = useSettings();
+  const { isAuthenticated } = useUser();
   const { company_logo, company_name } = settings ?? {};
 
   return (
     <StyledLogo>
-      <Img src={company_logo} alt="Your Company Logo Here" />
+      <Img 
+        className="mb-4"
+        src={company_logo} 
+        alt="Your Company Logo Here" />
       <TitleLogo>
         <h1>{company_name || `Your company name`}</h1>
-        {settings && Object.keys(settings).length > 0 && <EditIcon />}
+        {isAuthenticated && settings && Object.keys(settings).length > 0 && <EditIcon />}
       </TitleLogo>
     </StyledLogo>
   );
