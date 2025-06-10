20b9cf63f7d3cd62d2b1b305574e0dfbebe3f742
adriantech-beep <Adhrianne29@gmail.com>
2025-06-10 15:52:50 +0800

    Fixed the forms on Settings Page

--- src/settings/ColorPicker.jsx
+++ src/settings/ColorPicker.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
-
 function ColorPicker({ register, columnName, id, name }) {
-  const [color, setColor] = useState("");
-
   return (
     <input
       type="color"
       id={id}
       name={name}
-      {...register(columnName)}
-      value={color}
-      onChange={(e) => setColor(e.target.value)}
+      {...register(columnName, {
+        required: true,
+        pattern: {
+          value: /^#[0-9A-Fa-f]{6}$/,
+          message: "Invalid color format. Use #rrggbb",
+        },
+      })}
+      defaultValue="#ffffff"
     />
   );
 }
